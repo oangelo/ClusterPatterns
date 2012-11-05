f100b95daf12145d059ed71fe79dab27d6890023
oangelo <oangelo@gmail.com>
2012-11-05 09:49:37 -0200

    added patterns class interface
    removed old functions from TimeSeries that calculates cluster types
    create a interface for a function that should identify the cluster type

--- patterns.h
+++ patterns.h
@@ -2,24 +2,37 @@
 #define PATTERNS_H 
 
 #include <iostream> 
+#include <string> 
+
 #include "burn.h"
+
 
 //##################### Diagonal ###########################
 unsigned MainDiagonalLength(PairsList  cluster);
 unsigned SecondaryDiagonalLength(PairsList  cluster);
-//points that form diagonals lines
-template<class Type>
-std::vector<unsigned> PointsInDiagonal(Type data);
-template<class Type>
-std::vector<int> PointsInDiagonalDistances(Type data);
-
 
 //##################### Vertical ##########################
 unsigned VerticalLength(PairsList  cluster);
 unsigned HorizontalLength(PairsList  cluster);
-//points that form vertical lines
-template<class Type>
-std::vector<unsigned> PointsInVertical(Type data);
+
+std::string ClusterType(PairsList cluster);
+
+template <class Type>
+ class patterns{
+     public:
+        patterns(Type data);
+
+        std::vector<PairsList> main_diagonals_cluster;
+        std::vector<PairsList> secondary_diagonals_cluster;
+        std::vector<PairsList> verticals_cluster;
+        std::vector<PairsList> horizontal_cluster;
+        std::vector<PairsList> rectangle_cluster;
+        std::vector<PairsList> cross_cluster;
+        std::vector<unsigned> main_diagonals_length;
+        std::vector<unsigned> secondary_diagonals_length;
+        std::vector<unsigned> vertical_length;
+        std::vector<unsigned> horizontal_length;
+};
 
 #include "patterns_template.cpp" 
 
